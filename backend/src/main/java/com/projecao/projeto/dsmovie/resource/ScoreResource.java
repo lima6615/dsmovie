a585fe521e909842b60fe9f0f4c95a6b54b83426
lima6615 <leo-queiroz1@outlook.com>
2022-06-23 09:49:44 -0300

    save ,useNavigation

--- backend/src/main/java/com/projecao/projeto/dsmovie/resource/ScoreResource.java
+++ backend/src/main/java/com/projecao/projeto/dsmovie/resource/ScoreResource.java
@@ -1,6 +1,7 @@
 package com.projecao.projeto.dsmovie.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.projecao.projeto.dsmovie.service.ScoreService;
 
 @RestController
 @RequestMapping(value = "/scores")
+@CrossOrigin("*")
 public class ScoreResource {
 
 	@Autowired
